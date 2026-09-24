0e0dcff8ac907cae899004caaa11ada8e1e868d2
Tattsuwo <128314885+Tattsuwo@users.noreply.github.com>
2026-09-24 21:03:13 +0200

    Update HistoryScreen.kt

--- app/src/main/java/com/example/timetracker/ui/history/HistoryScreen.kt
+++ app/src/main/java/com/example/timetracker/ui/history/HistoryScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.stickyHeader
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.Card
 import androidx.compose.material3.HorizontalDivider
